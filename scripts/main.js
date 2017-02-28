31b507e14a5b65ba76e47b46ea6fe3cda2b97e13
future <futurestartslow@gmail.com>
2017-02-27 22:54:36 -0500

    Added a second image of Ed and updated main.js to include variable header depending on name of user

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/edcb.jpg') {
+      myImage.setAttribute ('src','images/ed_unmasked.png');
+    } else {
+      myImage.setAttribute ('src','images/edcb.jpg');
+    }
+}
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to Ed\'s, ' + myName;
+}
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hey! Welcome back to Ed\'s, ' + storedName;
+}
+
+myButton.onclick =  function() {
+  setUserName();
+}
